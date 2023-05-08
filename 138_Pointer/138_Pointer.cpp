407fe1dba4743caf64913c190bcdc96586b5ab67
NilaAyu1611 <nilaayucw@gmail.com>
2023-05-08 10:47:42 +0700

    membuat kelas mahasiswa

--- 138_Pointer/138_Pointer.cpp
+++ 138_Pointer/138_Pointer.cpp
@@ -1,10 +1,6 @@
-// 138_Pointer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class mahasiswa {
 
+};
